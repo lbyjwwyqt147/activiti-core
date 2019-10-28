5c494462efe1127d5e0a272b61786abfa6ba37d9
lbyjwwyqt147 <ljy671018>
2019-10-28 16:32:07 +0800

    init

---
+++ src/main/java/pers/liujunyi/cloud/activiti/controller/A.java
@@ -0,0 +1,4 @@
+package pers.liujunyi.cloud.activiti.controller;
+
+public class A {
+}
---
+++ src/test/java/pers/liujunyi/cloud/activiti/Test.java
@@ -0,0 +1,9 @@
+package pers.liujunyi.cloud.activiti;
+
+public class Test {
+
+    public static void main(String[] strings) {
+
+    }
+
+}
